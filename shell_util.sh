0ae14fe3ec950eee23341a13d08674d2a4d57476
Chi-Chun Liu <33436121+ChiChunLiu@users.noreply.github.com>
2019-03-01 10:29:07 -0600

    Update shell_util.sh

--- shell_util.sh
+++ shell_util.sh
@@ -2,4 +2,8 @@
 awk -vOFS=$'\t' '{print $2,$4,$5","$6}' file.snp | bgzip -c > file.tsv.gz
 tabix -s1 -b2 -e2 file.tsv.gz
 
-
+# AncestryMap dedup SNP
+# merge snp file with geno file
+paste $1.snp $1.geno >> tmp.snp_geno
+# prepend line-num, find dup, cut line-num out, print even line
+nl $1 | uniq -f1 -D | cut -f1 | sed -n 'n;p'
