406e7764a1e35a03b2d1a70b869e87057571f6db
Chi-Chun Liu <33436121+ChiChunLiu@users.noreply.github.com>
2019-03-07 02:01:14 -0600

    Update shell_util.sh

--- shell_util.sh
+++ shell_util.sh
@@ -7,3 +7,6 @@ tabix -s1 -b2 -e2 file.tsv.gz
 paste $1.snp $1.geno >> tmp.snp_geno
 # prepend line-num, find dup, cut line-num out, print even line
 nl $1 | uniq -f1 -D | cut -f1 | sed -n 'n;p'
+
+# print if a column value equals
+awk '$1 == "findtext" {print $3}'
